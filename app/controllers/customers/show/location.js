a078921b45f6a6546242a840b0442768ffa82a10
Vinh Luu <vinhlt3@gmail.com>
2016-08-28 00:48:09 +0700

    Vinhlt3/ch89/customer should show products only (#43)
    * implement: Customer should show products only
    
    * fix test run

--- app/controllers/customers/show/location.js
+++ app/controllers/customers/show/location.js
@@ -1,16 +1,17 @@
-import Ember from 'ember';
-import computed from 'ember-computed-decorators';
-import AddressValidations from 'last-strawberry/validators/address';
+import Ember from "ember";
+import computed from "ember-computed-decorators";
+import AddressValidations from "last-strawberry/validators/address";
 
 export default Ember.Controller.extend({
   AddressValidations,
 
-  @computed('items', 'model.itemDesires', 'model.itemCreditRates')
+  @computed("items", "model.itemDesires", "model.itemCreditRates")
   itemSettings(items, itemDesires, itemCreditRates) {
     return items
+      .filter(i => i.get("isProduct"))
       .map(item => {
-        const itemDesire = itemDesires.find(itemDesire => itemDesire.get('item.id') === item.get('id'));
-        const itemCreditRate = itemCreditRates.find(itemCreditRate => itemCreditRate.get('item.id') === item.get('id'));
+        const itemDesire = itemDesires.find(itemDesire => itemDesire.get("item.id") === item.get("id"));
+        const itemCreditRate = itemCreditRates.find(itemCreditRate => itemCreditRate.get("item.id") === item.get("id"));
 
         return {
           item,
@@ -20,8 +21,8 @@ export default Ember.Controller.extend({
       });
   },
 
-  @computed('model.company.locations.@each.{locationHash}')
+  @computed("model.company.locations.@each.{locationHash}")
   addresses(locations) {
-    return locations.map(location => location.get('address'));
+    return locations.map(location => location.get("address"));
   }
 });
