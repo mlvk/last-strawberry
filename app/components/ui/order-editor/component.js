8bb5b12261ce79c98390ad14d137073c225dc478
Vinh Luu <vinhlt3@gmail.com>
2016-08-28 00:48:51 +0700

    disable creat an new item on sales order page (#42)

--- app/components/ui/order-editor/component.js
+++ app/components/ui/order-editor/component.js
@@ -1,55 +1,55 @@
-import Ember from 'ember';
-import downloadFile from 'last-strawberry/utils/download-file';
-import computed from 'ember-computed-decorators';
+import Ember from "ember";
+import downloadFile from "last-strawberry/utils/download-file";
+import computed from "ember-computed-decorators";
 
 const { alias, not } = Ember.computed;
 
 export default Ember.Component.extend({
-  classNames:       ['section_sales-order_order-editor', 'col'],
+  classNames:       ["section_sales-order_order-editor", "col"],
 
   pdfGenerator:     Ember.inject.service(),
 
-  company:          alias('model.location.company'),
-  isSalesOrder:     alias('model.isSalesOrder'),
-  isPurchaseOrder:  not('isSalesOrder'),
+  company:          alias("model.location.company"),
+  isSalesOrder:     alias("model.isSalesOrder"),
+  isPurchaseOrder:  not("isSalesOrder"),
 
-  @computed('itemSearchString')
+  @computed("itemSearchString")
   noMatchesMessage(str) {
-    return `Create new item: ${str}`;
+    return this.get("isPurchaseOrder")? `Create new item: ${str}`: `Item not found: ${str}`;
   },
 
   actions: {
     printOrder() {
-      return this.get('pdfGenerator')
-        .generateInvoices([this.get('model')])
-        .then(pdfData => downloadFile(pdfData.url, `${this.get('model.orderNumber')}.pdf`))
+      return this.get("pdfGenerator")
+        .generateInvoices([this.get("model")])
+        .then(pdfData => downloadFile(pdfData.url, `${this.get("model.orderNumber")}.pdf`))
         .catch(err => err);
     },
 
     createOrderItem(item) {
-      this.set('customAddItemResult', undefined);
+      this.set("customAddItemResult", undefined);
       this.attrs.createOrderItem(item);
     },
 
     stashItemSearch(str) {
-      this.set('itemSearchString', str);
+      this.set("itemSearchString", str);
     },
 
     onItemSearchKeyDown(obj, keyboard) {
-      if(keyboard.code === 'Enter' && !obj.highlighted) {
-        this.set('showCreateItemModal', true);
+      if(keyboard.code === "Enter" && this.get("isPurchaseOrder") && !obj.highlighted) {
+        this.set("showCreateItemModal", true);
       }
     },
 
     cancelCreateNewItem() {
-      this.set('showCreateItemModal', false);
+      this.set("showCreateItemModal", false);
     },
 
     async requestCreateNewItem(formData) {
       const newItem = await this.attrs.createNewItem(formData);
 
       this.attrs.createOrderItem(newItem);
-      this.set('showCreateItemModal', false);
+      this.set("showCreateItemModal", false);
     }
   }
 });
