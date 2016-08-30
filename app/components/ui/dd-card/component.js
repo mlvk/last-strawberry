a99e2df74dbb0df386aae69835621f1a363ad1b5
Vinh Luu <vinhlt3@gmail.com>
2016-08-31 02:07:25 +0700

    make open order the original yellow color (#56)

--- app/components/ui/dd-card/component.js
+++ app/components/ui/dd-card/component.js
@@ -1,15 +1,15 @@
-import Ember from 'ember';
-import { style } from 'last-strawberry/utils/styles';
-import colors from 'last-strawberry/constants/colors';
+import Ember from "ember";
+import { style } from "last-strawberry/utils/styles";
+import colors from "last-strawberry/constants/colors";
 
 export default Ember.Component.extend({
-  classNames: ['col'],
-  attributeBindings:['componentStyles:style'],
+  classNames: ["col"],
+  attributeBindings:["componentStyles:style"],
 
-  @style('colorScheme.backgroundColor')
-  componentStyles(backgroundColor = colors.SKY_BLUE) {
+  @style("colorScheme.backgroundColor")
+  componentStyles(backgroundColor = colors.DARK_ORANGE) {
     return {
-      'background-color': backgroundColor
+      "background-color": backgroundColor
     };
   }
 });
--- app/components/ui/dd-card/dd-card-body/component.js
+++ app/components/ui/dd-card/dd-card-body/component.js
@@ -1,15 +1,14 @@
 import Ember from "ember";
 import { style } from "last-strawberry/utils/styles";
-import COLOR_SCHEMES from 'last-strawberry/constants/color-schemes';
+import Colors from "last-strawberry/constants/colors";
 
 export default Ember.Component.extend({
   classNames: ["row"],
   attributeBindings:["componentStyles:style"],
 
-  @style("index")
-  componentStyles(index = 0) {
-    return {
-      "color": COLOR_SCHEMES[index].color
-    };
+  @style("colorScheme")
+  componentStyles(colorScheme) {
+    const color = colorScheme !== undefined? colorScheme.color: Colors.DARK_GREY;
+    return { color };
   }
 });
