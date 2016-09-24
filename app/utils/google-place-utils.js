ef3b69ebd4a127aaa6fde5f0bf5d3896be95a9c1
Vinh Luu <vinhlt3@gmail.com>
2016-09-24 17:47:45 +0700

    Vinhlt3/ch284/does not show error when input an invalid (#101)
    * show error when input an invalid address
    
    * fix test

--- app/utils/google-place-utils.js
+++ app/utils/google-place-utils.js
@@ -1,15 +1,13 @@
-import Ember from 'ember';
-const { isPresent } = Ember;
 const DEFAULT_ADDRESS_MAP = {
-  street_number: 'streetNumber',
-  route: 'streetName',
-  locality: 'city',
-  administrative_area_level_1: 'state',
-  postal_code: 'zip'
+  street_number: "streetNumber",
+  route: "streetName",
+  locality: "city",
+  administrative_area_level_1: "state",
+  postal_code: "zip"
 };
 
 const placeToObject = place => {
-  const components = _.get(place, 'address_components', []);
+  const components = _.get(place, "address_components", []);
   const mapped = Object.keys(DEFAULT_ADDRESS_MAP)
     .map(key => {
       return components
@@ -24,12 +22,13 @@ const placeToObject = place => {
     })
 
   const flattened = R.mergeAll(R.flatten(mapped));
-  const street = `${_.get(flattened, 'streetNumber', '')} ${_.get(flattened, 'streetName', '')}`.trim(),
-        lat = _.get(place, 'geometry.location.lat', () => {})(),
-        lng = _.get(place, 'geometry.location.lng', () => {})();
+  const street = `${_.get(flattened, "streetNumber", "")} ${_.get(flattened, "streetName", "")}`.trim(),
+        lat = _.get(place, "geometry.location.lat", () => {})(),
+        lng = _.get(place, "geometry.location.lng", () => {})();
 
   const final = Object.assign(flattened, { street, lat, lng });
-  return R.filter(isPresent, final);
+
+  return final;
 }
 
 export {
--- tests/unit/utils/google-place-utils-test.js
+++ tests/unit/utils/google-place-utils-test.js
@@ -1,13 +1,19 @@
-import { placeToObject } from 'last-strawberry/utils/google-place-utils';
-import { module, test } from 'qunit';
+import { placeToObject } from "last-strawberry/utils/google-place-utils";
+import { module, test } from "qunit";
 import {
   VALID_RESPONSE,
   INVALID_REPONSE_MISSING_STREET_INFO
-} from 'last-strawberry/tests/mocks/google-places';
+} from "last-strawberry/tests/mocks/google-places";
 
-module('Unit | Utils | google place utils');
+const INVALID_OBJECT = {
+  "lat": undefined,
+  "lng": undefined,
+  "street": ""
+};
 
-test('returns valid object when input place is valid', function(assert) {
+module("Unit | Utils | google place utils");
+
+test("returns valid object when input place is valid", function(assert) {
   const result = placeToObject(VALID_RESPONSE);
 
   assert.deepEqual(result, {
@@ -22,7 +28,7 @@ test('returns valid object when input place is valid', function(assert) {
   });
 });
 
-test('returns valid object when input place is valid', function(assert) {
+test("returns valid object when input place is valid", function(assert) {
   const result = placeToObject(INVALID_REPONSE_MISSING_STREET_INFO);
 
   assert.deepEqual(result, {
@@ -30,24 +36,25 @@ test('returns valid object when input place is valid', function(assert) {
     "lat": 42.3467625,
     "lng": -71.06871519999999,
     "state": "MA",
-    "zip": "02116"
+    "zip": "02116",
+    "street": ""
   });
 });
 
-test('returns empty object when empty place is passed', function(assert) {
+test("returns empty object when empty place is passed", function(assert) {
   const result = placeToObject({});
 
-  assert.deepEqual(result, {});
+  assert.deepEqual(result, INVALID_OBJECT);
 });
 
-test('returns empty object when undefined is passed', function(assert) {
+test("returns empty object when undefined is passed", function(assert) {
   const result = placeToObject(undefined);
 
-  assert.deepEqual(result, {});
+  assert.deepEqual(result, INVALID_OBJECT);
 });
 
-test('returns empty object when string is passed', function(assert) {
-  const result = placeToObject('xxx');
+test("returns empty object when string is passed", function(assert) {
+  const result = placeToObject("xxx");
 
-  assert.deepEqual(result, {});
+  assert.deepEqual(result, INVALID_OBJECT);
 });
